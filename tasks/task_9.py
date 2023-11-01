c0f28e682d472cd37cb24be26038d616b06ecc47
DemetrS <tanastr5387@gmail.com>
2023-11-01 18:55:10 +0600

    задания 6,8,9

--- tasks/task_9.py
+++ tasks/task_9.py
@@ -1,7 +1,10 @@
 byte_size = 1234567890  # объем информации в байтах
 
-kilo_size = ...  # здесь Ваш код
-mega_size = ...  # здесь Ваш код
-giga_size = ...  # здесь Ваш код
+kilo_size = byte_size / 1024
+mega_size = kilo_size /1024
+giga_size = mega_size / 1024 
 
-print(...)  # допишите код
+print("Размер в байах:", byte_size)
+print("Размер в килобайах:", kilo_size)
+print("Размер в мегабайах:", mega_size)
+print("Размер в гигабайах:", giga_size)
